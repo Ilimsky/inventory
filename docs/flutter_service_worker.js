7300a285ba6ad25877c5cf1e60683c03d141e4ed
Ilimsky <ilim.sarykbaev@gmail.com>
2026-02-16 05:19:44 +0600

    добавлено описание в qr код 16.02.2026 05:19

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "43338579a56b579f7cf04d7519e31f90
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ec6ddcdf2589d8edc7cafe6d80c68b7f",
+"flutter_bootstrap.js": "f4e18028a7b0eb8c8205d73d6b91f0bd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1cd237f52561d13f698fec49221a8dcd",
 "/": "1cd237f52561d13f698fec49221a8dcd",
-"main.dart.js": "be799196ca5b8050df88d0110a8a2ea4",
+"main.dart.js": "470d5c9804c3e24caeb208c9ecc575a9",
 "manifest.json": "dd647aed3900efeada55c9e0a7d277ad",
 "version.json": "c8bbbd91228c283f0d8d17f9aba0d14f"};
 // The application shell files that are downloaded before a service worker can
